edb1872dbe6944f2466479ecc93feda5ade7ed6d
metamuse-virtualidol <metamuse.virtualidol@gmail.com>
2024-07-15 13:40:47 +0900

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// トップへ戻る
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// 音楽再生
+document.addEventListener('DOMContentLoaded', function () {
+    var audioElements = document.querySelectorAll('audio');
+    audioElements.forEach(function(audio) {
+        audio.addEventListener('play', function() {
+            audioElements.forEach(function(otherAudio) {
+                if (otherAudio !== audio) {
+                    otherAudio.pause();
+                    otherAudio.currentTime = 0;
+                }
+            });
+        });
+    });
+});
